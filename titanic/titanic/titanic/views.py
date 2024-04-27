e6f657bd5874479d2115341f180c0aa191f51eba
Bobur Yusupov <bobur.yu@anur.tech>
2024-04-27 08:23:29 +0500

    Deployed model using Django

--- titanic/titanic/titanic/views.py
+++ titanic/titanic/titanic/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.http.request import HttpRequest
 import pickle
 import pandas as pd
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_dir, "log_reg_model.pkl")
 
 # Create your views here.
-with open("log_reg_model.pkl", 'rb') as f:
+with open(model_path, 'rb') as f:
     model = pickle.load(f)
 
 def predict(request: HttpRequest):
@@ -20,5 +24,9 @@ def predict(request: HttpRequest):
         X["sex"] = X["sex"].map({"male": 0, "female": 1})
 
         prediction = model.predict(X)
+        
+        # Convert prediction to boolean value (True if survived, False otherwise)
+        survived = bool(prediction[0])
 
-        return render
+        # Pass the predicted value to the template using a context dictionary
+        return render(request, 'index.html', {'survived': survived})
