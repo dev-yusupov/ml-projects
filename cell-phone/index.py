39da7965c83c4f70f8cf1ce32c2ea4396ebf1911
Bobur Yusupov <bobur.yu@anur.tech>
2024-05-01 14:41:15 +0500

    Moved to deep learning folder

--- cell-phone/index.py
+++ cell-phone/index.py
@@ -4,3 +4,4 @@ dataset = pd.read_csv("train_data.csv")
 pd.set_option("display.max_columns", None)
 
 columns = ['battery_power', 'clock_speed', 'dual_sim', 'fc', 'four_g', 'int_memory', 'm_dep', ]
+
